99c47b17b20bcc9689cb88093885ef59fe840474
Watersfall <dontshoot262@yahoo.com>
2021-03-04 20:33:11 -0500

    Initial commit

---
+++ src/main/java/net/watersfall/minefabrictory/MineFabrictory.java
@@ -0,0 +1,12 @@
+package net.watersfall.minefabrictory;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MineFabrictory implements ModInitializer
+{
+	@Override
+	public void onInitialize()
+	{
+
+	}
+}
---
+++ src/main/java/net/watersfall/minefabrictory/client/MineFabrictoryClient.java
@@ -0,0 +1,15 @@
+package net.watersfall.minefabrictory.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class MineFabrictoryClient implements ClientModInitializer
+{
+	@Override
+	public void onInitializeClient()
+	{
+
+	}
+}
